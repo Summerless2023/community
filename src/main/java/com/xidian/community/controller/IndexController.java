238da5b7998d46e4c49b0688ceca93d0879bebbe
张一帆 <zhangyifan@zhangyifandeMacBook-Pro.local>
2019-12-29 18:01:50 +0800

    测试

--- src/main/java/com/xidian/community/controller/IndexController.java
+++ src/main/java/com/xidian/community/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.xidian.community.controller;
 import com.xidian.community.mapper.UserMapper;
 import com.xidian.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,9 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 public class IndexController {
     @Autowired
     private UserMapper userMapper;
-
+    @Value("${com.env.name}")
+    private String name;
     @GetMapping("/")
     public String hello(HttpServletRequest request) {
+        System.out.println("name = "+ name);
         Cookie[] cookies = request.getCookies();
         for (Cookie cookie :
                 cookies) {
